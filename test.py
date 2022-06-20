b17901c79455230d75ebb0f020185452f7fd5277
amr lotfy <amroahmed1357@gmail.com>
2022-06-21 01:51:34 +0200

    test function file

---
+++ test.py
@@ -0,0 +1,7 @@
+def who_is_greater(a,b):
+    if b > a:
+        print(b , "is greater than", a)
+    elif a == b:
+        print(a, "and", b, "are equal")
+    else:
+        print(a, "is greater than", b)
